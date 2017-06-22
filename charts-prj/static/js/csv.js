3e939477b699c817576e55157496f45ef3d694d9
Emere <emere.arco@gmail.com>
2017-06-22 15:20:02 +0200

    Line chart added

--- charts-prj/static/js/csv.js
+++ charts-prj/static/js/csv.js
@@ -1,6 +1,7 @@
 var getCsv = function(csv_link, category, quantity, agg, chartType){
                var data = [];
-               d3.csv(csv_link, function(csv) {
+               d3.csv(csv_link, function(error, csv) {
+                        if (error) throw error;
                         csv.forEach(function(row){
                         var qnty_v; var ctgy_v; var i = 0;
                             while (i < Object.keys(row).length){
@@ -56,6 +57,37 @@ var getCsv = function(csv_link, category, quantity, agg, chartType){
                        .rollup(function(v) { return v.length;})
                        .entries(dato);
                };
+
+
+               // Alternate sorting
+               function alt_sort(dato){
+                    dato_sort = [];
+                    do {
+                    var lowest = Number.POSITIVE_INFINITY; var low_cat; var min_obj={ key: "", value: 0};
+                    var highest = Number.NEGATIVE_INFINITY; var high_cat; var max_obj={ key: "", value: 0};
+                    var tmp; var tmp_cat;
+                    for (var i=dato.length-1; i>=0; i--) {
+                        tmp = dato[i].value; min_obj.value = tmp;
+                        tmp_cat = dato[i].key; min_obj.key = tmp_cat;
+                        if (dato.length == 1) {
+                        tmp = dato[i].value;
+                        tmp_cat = dato[i].key;
+                        dato_sort.push(min_obj);
+                        dato.pop();
+                        break;
+                        }
+                        if (tmp < lowest) {lowest = tmp; min_obj.key = tmp_cat; min_obj.value = tmp }
+                        if (tmp > highest) {highest = tmp; max_obj.key = tmp_cat; max_obj.value = tmp}
+                    }console.log(highest, lowest, min_obj, max_obj, dato);
+                    dato_sort.push(min_obj);
+                    dato.pop();
+                    dato_sort.push(max_obj);
+                    dato.shift();
+                    } while (dato.length > 0)
+                    console.log(dato_sort)
+                    return dato_sort;
+               }
+
 
                function descendent(a,b){
                 if (a.value < b.value) return 1;
@@ -115,6 +147,17 @@ var getCsv = function(csv_link, category, quantity, agg, chartType){
                    d3.select('#chart_area')
                       .datum(dati)
                       .call(donut);
+               } else if (chartType == 3){
+                   if (dati.length > 10){
+                        while (dati.length >= 10) { dati.pop(); };
+                        window.alert("Output limited to 10 categories");
+                   };
+                   var line = lineChart(category, quantity, title)
+                   .variable('value')
+                   .category('key')
+                   d3.select('#chart_area')
+                      .datum(dati)
+                      .call(line);
                }
 
                });
---
+++ charts-prj/static/js/line.js
@@ -0,0 +1,205 @@
+function lineChart(cat, qnt, title){
+        var width = 1200,
+            height = 600,
+            padding = 5,
+            xScale = d3.scaleBand(),
+            yScale = d3.scaleLinear(),
+            colour = d3.scaleOrdinal(d3.schemeCategory20),
+            x,
+            y,
+            margin = { top: 30, bottom: 30, left: 30, right: 120 },
+            floatFormat = d3.format("." + d3.precisionFixed(0.5) + "f"),
+            xAxis = d3.axisBottom(xScale),
+            yAxis = d3.axisLeft(yScale);
+
+        function my(selection){
+
+          selection.each(function(data) {
+
+            var svg = selection.append('svg')
+                .attr("width", width)
+                .attr("height", height);
+
+            var g = svg.selectAll("g")
+              .data([1]);
+            g = g.enter().append("g")
+              .merge(g)
+                .attr("transform",
+                      "translate(" + margin.left + "," + margin.top +")");
+
+
+            var innerWidth = width - margin.left - margin.right ;
+            var innerHeight = function(){ return height - margin.top - margin.bottom} ;
+
+
+            xScale
+              .domain(data.map(function (d){ return d[x]; }))
+              .range([0, (innerWidth - margin.right)]);
+
+
+            // X axis
+            var xAxisG = g.selectAll(".x-axis").data([1]);
+            xAxisG.enter().append("g")
+                .attr("class", "x-axis")
+              .merge(xAxisG)
+                .attr("transform", "translate(0," + innerHeight() +")")
+                .call(xAxis);
+
+            // labels X axis
+            // rotate text if is longer than...
+
+            d3.select(this).select(".x-axis").selectAll("text").each(function(){
+                if (this.getBBox().width > (xScale.bandwidth() - (padding*2)))
+                    d3.selectAll("text").attr("transform", "rotate(-90)")
+                                        .attr("y", 0)
+                                        .attr("x", -10)
+                                        .attr("dy", ".35em")
+                                        .style("text-anchor", "end");
+            });
+
+            // adjust margin and x axis title
+            var maxh = 0;
+            d3.select(this).select(".x-axis").selectAll("text").each(function(){
+                if (this.getBBox().width > maxh)
+                    maxh = this.getBBox().width;
+            });
+            margin.bottom = margin.bottom + maxh;
+            d3.select(".x-axis").attr("transform", "translate(0," + (innerHeight()) + ")");
+
+
+            g.append("text")
+                .attr("transform", "translate(" + ((innerWidth/2) - margin.left) + ", " + (innerHeight() + margin.bottom) + ")")
+                .style("font-size", "12px")
+                .style("text-anchor", "middle")
+                .text(cat);
+
+            yScale
+              .domain([0, d3.max(data, function (d){ return d[y] ;})])
+              .range([innerHeight(), 0]);
+
+            // Y axis
+            var yAxisG = g.selectAll(".y-axis").data([1]);
+            yAxisG.enter().append("g")
+                .attr("class", "y-axis")
+               .merge(yAxisG)
+               .call(yAxis);
+
+            //labels Y axis
+            var maxw = 0;
+            d3.select(this).select(".y-axis").selectAll("text").each(function(){
+                if (this.getBBox().width > maxw) {
+                    maxw = this.getBBox().width;
+                }
+            });
+            margin.left = margin.left + maxw;
+            g.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var yLabel = function(){
+                         if (title == "Count"){
+                            return "Count";
+                         } else {
+                            return qnt;
+                         }};
+
+            g.append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("x", 0 - (innerHeight()/2))
+                .attr("y", 0 - (margin.left))
+                .attr("dy", "1em")
+                .style("font-size", "12px")
+                .style("text-anchor", "middle")
+                .text(yLabel);
+
+            // bars
+            var line = d3.line()
+                        .x(function(d) { return xScale(d[x]); })
+                        .y(function(d) { return yScale(d[y]); });
+
+             g.append("path")
+              .data(data)
+              .attr("fill", "none")
+              .attr("stroke", "steelblue")
+              .attr("stroke-linejoin", "round")
+              .attr("stroke-linecap", "round")
+              .attr("stroke-width", 1.5)
+              .attr("d", line);
+
+
+           /* var rects = g.selectAll("rect")
+              .data(data);
+            rects.exit().remove();
+            rects.enter().append("rect")
+              .merge(rects)
+                .attr("x", function (d){ return xScale(d[x]) + padding; })
+                .attr("y", function (d){ return yScale(d[y]); })
+                .attr("fill", function(d) { return colour(xScale(d[x])); })
+                .attr("width", xScale.bandwidth() - (padding*2))
+                .attr("height", function (d){
+                  return innerHeight() - yScale(d.value);
+                });*/
+
+
+            //chart title
+            var chart_title = function(){
+                                if (title == "Count"){
+                                    return title + " of elements for "  + cat;
+                                } else {
+                                  return title + " of " + qnt + " for " + cat;
+                                }};
+
+            svg.append("text")
+                .attr("x", (width / 2))
+                .attr("y", 0 + (margin.top / 2))
+                .attr("text-anchor", "middle")
+                .style("font-size", "16px")
+                .style("text-decoration", "underline")
+                .text(chart_title);
+
+            //chart legend
+            var legend = svg.selectAll(".legend")
+                .data(data)
+                .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+            legend.append("rect")
+                .attr("x", width - 18)
+                .attr("width", 18)
+                .attr("height", 18)
+                .style("fill", function(d) { return colour(xScale(d[x]))} );
+
+            legend.append("text")
+                .attr("x", width - 24)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "end")
+                .text(function (d){ return d[x] + ": " + floatFormat(d[y]); });
+
+
+          });
+        }
+
+        my.x = function (_){
+          return arguments.length ? (x = _, my) : x;
+        };
+
+        my.y = function (_){
+          return arguments.length ? (y = _, my) : y;
+        };
+
+        my.width = function (_){
+          return arguments.length ? (width = _, my) : width;
+        };
+
+        my.height = function (_){
+          return arguments.length ? (height = _, my) : height;
+        };
+
+        my.padding = function (_){
+          return arguments.length ? (xScale.padding(_), my) : xScale.padding();
+        };
+
+        return my;
+      }
+
+
