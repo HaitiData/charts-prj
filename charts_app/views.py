e4492d0d3827b9f48a7786197b044eacc9945b25
sbsimo <simone.blb@gmail.com>
2017-06-30 17:49:23 +0200

    require login and store username

--- charts_app/views.py
+++ charts_app/views.py
@@ -1,9 +1,6 @@
-import csv
-
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
 
 from geonode.layers.models import Layer
 
@@ -11,11 +8,18 @@ from .models import Chart, ChartForm
 from wfs_harvest.utils import get_fields
 
 
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
+
+
 class ChartDetailView(DetailView):
     model = Chart
 
 
-class ChartCreate(CreateView):
+class ChartCreate(LoginRequiredMixin, CreateView):
     form_class = ChartForm
     template_name = 'charts_app/chart_form.html'
 
@@ -35,38 +39,19 @@ class ChartCreate(CreateView):
         ctx['layer'] = layer
         return ctx
 
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super(ChartCreate, self).form_valid(form)
 
-class ChartUpdate(UpdateView):
+
+class ChartUpdate(LoginRequiredMixin, UpdateView):
     model = Chart
     fields = '__all__'
     template_name_suffix = '_update_form'
 
 
-class ChartDelete(DeleteView):
+class ChartDelete(LoginRequiredMixin, DeleteView):
     model = Chart
     success_url = '/'
 
 
-def pie_chart_v1(request):
-    return render(request, 'pie_chart_v1.html')
-
-
-def donut_chart_v1(request):
-    return render(request, 'donut_chart_v1.html')
-
-
-def pie_chart_v2(request):
-    qdict = request.GET
-    typename = qdict['lyrname']
-    category_field = qdict['category']
-    quantity_field = qdict['quantity']
-    context = {
-        'lyrname': typename,
-        'category': category_field,
-        'quantity': quantity_field
-    }
-    return render(request, 'pie_chart_v2.html', context)
-
-
-def donut_chart_v2(request):
-    return render(request, 'donut_chart_v2.html')
